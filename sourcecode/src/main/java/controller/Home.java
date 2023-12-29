b70c4c77b1517b0df33a0778e68c6b8bf549fe48
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2023-12-29 23:13:52 +0700

    refactor: moved files into 'java' package to run maven build

---
+++ sourcecode/src/main/java/controller/Home.java
@@ -0,0 +1,193 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Pane;
+
+public class Home {
+
+    @FXML
+    private Menu aboutMenu;
+
+    @FXML
+    private Menu helpMenu;
+
+    @FXML
+    private Button key001;
+
+    @FXML
+    private Button key002;
+
+    @FXML
+    private Button key003;
+
+    @FXML
+    private Button key004;
+
+    @FXML
+    private Button key005;
+
+    @FXML
+    private Button key006;
+
+    @FXML
+    private Button key007;
+
+    @FXML
+    private Button key008;
+
+    @FXML
+    private Button key009;
+
+    @FXML
+    private Button key010;
+
+    @FXML
+    private Button key011;
+
+    @FXML
+    private Button key012;
+
+    @FXML
+    private Button key013;
+
+    @FXML
+    private Button key014;
+
+    @FXML
+    private Button key015;
+
+    @FXML
+    private Button key016;
+
+    @FXML
+    private Button key017;
+
+    @FXML
+    private Button key018;
+
+    @FXML
+    private Button key019;
+
+    @FXML
+    private Button key020;
+
+    @FXML
+    private Button key021;
+
+    @FXML
+    private Button key022;
+
+    @FXML
+    private Button key023;
+
+    @FXML
+    private Button key024;
+
+    @FXML
+    private Button key025;
+
+    @FXML
+    private Button key026;
+
+    @FXML
+    private Button key027;
+
+    @FXML
+    private Button key028;
+
+    @FXML
+    private Button key029;
+
+    @FXML
+    private Button key030;
+
+    @FXML
+    private Button key031;
+
+    @FXML
+    private Button key032;
+
+    @FXML
+    private Button key033;
+
+    @FXML
+    private Button key034;
+
+    @FXML
+    private Button key035;
+
+    @FXML
+    private Button key036;
+
+    @FXML
+    private Button key037;
+
+    @FXML
+    private Button key038;
+
+    @FXML
+    private Button key039;
+
+    @FXML
+    private Button key040;
+
+    @FXML
+    private Button key041;
+
+    @FXML
+    private Button key042;
+
+    @FXML
+    private Button key043;
+
+    @FXML
+    private Button key044;
+
+    @FXML
+    private Button key045;
+
+    @FXML
+    private Button key046;
+
+    @FXML
+    private Button key047;
+
+    @FXML
+    private Button key048;
+
+    @FXML
+    private Button key049;
+
+    @FXML
+    private Button key050;
+
+    @FXML
+    private Button key051;
+
+    @FXML
+    private Button key052;
+
+    @FXML
+    private Button key053;
+
+    @FXML
+    private Button key054;
+
+    @FXML
+    private Button key055;
+
+    @FXML
+    private ToggleGroup musicStyleToggle;
+
+    @FXML
+    private Pane pianoPane;
+
+    @FXML
+    private Slider volumeSlider;
+
+
+}
