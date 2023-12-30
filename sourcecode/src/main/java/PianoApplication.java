ecb62f66429aaa4f29cc012429efd55a1c620742
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2023-12-30 18:33:00 +0700

    move controller binding to fxml file

--- sourcecode/src/main/java/PianoApplication.java
+++ sourcecode/src/main/java/PianoApplication.java
@@ -7,11 +7,9 @@ public class PianoApplication {
     public static class AppUI extends Application {
         @Override
         public void start(javafx.stage.Stage primaryStage) throws Exception {
-            final String HOME_FXML_FILE_PATH = "fxml/Home.fxml";
+            final String HOME_FXML_FILE_PATH = "/fxml/Home.fxml";
             System.out.println(getClass().getResource(HOME_FXML_FILE_PATH));
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(HOME_FXML_FILE_PATH));
-            Home home = new Home();
-            fxmlLoader.setController(home);
             Parent root = fxmlLoader.load();
 
             Scene scene = new Scene(root);
